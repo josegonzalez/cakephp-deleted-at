d16a76e3602f31b5f6f46e96f226ff6ea1063e2c
Jose Diaz-Gonzalez <myaccounts@savant.be>
2013-12-21 17:12:25 -0500

    Add softdelete() and undelete() methods

--- Model/Behavior/DeletedAtBehavior.php
+++ Model/Behavior/DeletedAtBehavior.php
@@ -34,5 +34,53 @@ class DeletedAtBehavior extends ModelBehavior {
       return $results;
   }
 
+  public function softdelete(Model $model, $id = null) {
+    if ($id) {
+      $model->id = $id;
+    }
+
+    if (!$model->id) {
+      return false;
+    }
+
+    $deleteCol = 'deleted';
+    if (!$model->hasField($deleteCol)) {
+      return false;
+    }
+
+    $db = $model->getDataSource();
+    $now = time();
+
+    $default = array('formatter' => 'date');
+    $colType = array_merge($default, $db->columns[$model->getColumnType($deleteCol)]);
+
+    $time = $now;
+    if (array_key_exists('format', $colType)) {
+      $time = call_user_func($colType['formatter'], $colType['format']);
+    }
+
+    if (!empty($model->whitelist)) {
+      $model->whitelist[] = $deleteCol;
+    }
+    $model->set($deleteCol, $time);
+    return $model->saveField($deleteCol, $time);
+  }
+
+  public function undelete(Model $model, $id = null) {
+    if ($id) {
+      $model->id = $id;
+    }
+
+    if (!$model->id) {
+      return false;
+    }
+
+    $deleteCol = 'deleted';
+    if (!$model->hasField($deleteCol)) {
+      return false;
+    }
+
+    $model->set($deleteCol, null);
+    return $model->saveField($deleteCol, null);
+  }
 }
-?>
