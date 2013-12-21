70d618b3c57b499894f2c3e66fe85ae5c6b772bd
Jose Diaz-Gonzalez <myaccounts@savant.be>
2013-12-21 22:23:33 +0000

    Update tests for phpcs

--- Model/Behavior/DeletedAtBehavior.php
+++ Model/Behavior/DeletedAtBehavior.php
@@ -2,85 +2,85 @@
 App::uses('ModelBehavior', 'Model');
 class DeletedAtBehavior extends ModelBehavior {
 
-  public $mapMethods = array(
-    '/findDeleted/' => 'findDeleted',
-    '/findNon_deleted/' => 'findNonDeleted',
-  );
+	public $mapMethods = array(
+		'/findDeleted/' => 'findDeleted',
+		'/findNon_deleted/' => 'findNonDeleted',
+	);
 
-  public function setup(Model $model, $config = array()) {
-      $model->findMethods['deleted'] = true;
-      $model->findMethods['non_deleted'] = true;
-  }
+	public function setup(Model $model, $config = array()) {
+			$model->findMethods['deleted'] = true;
+			$model->findMethods['non_deleted'] = true;
+	}
 
-  public function findDeleted(Model $model, $functionCall, $state, $query, $results = array()) {
-      if ($state == 'before') {
-        if (empty($query['conditions'])) {
-          $query['conditions'] = array();
-        }
-        $query['conditions']["{$model->alias}.deleted <>"] = null;
-        return $query;
-      }
-      return $results;
-  }
+	public function findDeleted(Model $model, $functionCall, $state, $query, $results = array()) {
+			if ($state == 'before') {
+				if (empty($query['conditions'])) {
+					$query['conditions'] = array();
+				}
+				$query['conditions']["{$model->alias}.deleted <>"] = null;
+				return $query;
+			}
+			return $results;
+	}
 
-  public function findNonDeleted(Model $model, $functionCall, $state, $query, $results = array()) {
-      if ($state == 'before') {
-        if (empty($query['conditions'])) {
-          $query['conditions'] = array();
-        }
-        $query['conditions']["{$model->alias}.deleted"] = null;
-        return $query;
-      }
-      return $results;
-  }
+	public function findNonDeleted(Model $model, $functionCall, $state, $query, $results = array()) {
+			if ($state == 'before') {
+				if (empty($query['conditions'])) {
+					$query['conditions'] = array();
+				}
+				$query['conditions']["{$model->alias}.deleted"] = null;
+				return $query;
+			}
+			return $results;
+	}
 
-  public function softdelete(Model $model, $id = null) {
-    if ($id) {
-      $model->id = $id;
-    }
+	public function softdelete(Model $model, $id = null) {
+		if ($id) {
+			$model->id = $id;
+		}
 
-    if (!$model->id) {
-      return false;
-    }
+		if (!$model->id) {
+			return false;
+		}
 
-    $deleteCol = 'deleted';
-    if (!$model->hasField($deleteCol)) {
-      return false;
-    }
+		$deleteCol = 'deleted';
+		if (!$model->hasField($deleteCol)) {
+			return false;
+		}
 
-    $db = $model->getDataSource();
-    $now = time();
+		$db = $model->getDataSource();
+		$now = time();
 
-    $default = array('formatter' => 'date');
-    $colType = array_merge($default, $db->columns[$model->getColumnType($deleteCol)]);
+		$default = array('formatter' => 'date');
+		$colType = array_merge($default, $db->columns[$model->getColumnType($deleteCol)]);
 
-    $time = $now;
-    if (array_key_exists('format', $colType)) {
-      $time = call_user_func($colType['formatter'], $colType['format']);
-    }
+		$time = $now;
+		if (array_key_exists('format', $colType)) {
+			$time = call_user_func($colType['formatter'], $colType['format']);
+		}
 
-    if (!empty($model->whitelist)) {
-      $model->whitelist[] = $deleteCol;
-    }
-    $model->set($deleteCol, $time);
-    return $model->saveField($deleteCol, $time);
-  }
+		if (!empty($model->whitelist)) {
+			$model->whitelist[] = $deleteCol;
+		}
+		$model->set($deleteCol, $time);
+		return $model->saveField($deleteCol, $time);
+	}
 
-  public function undelete(Model $model, $id = null) {
-    if ($id) {
-      $model->id = $id;
-    }
+	public function undelete(Model $model, $id = null) {
+		if ($id) {
+			$model->id = $id;
+		}
 
-    if (!$model->id) {
-      return false;
-    }
+		if (!$model->id) {
+			return false;
+		}
 
-    $deleteCol = 'deleted';
-    if (!$model->hasField($deleteCol)) {
-      return false;
-    }
+		$deleteCol = 'deleted';
+		if (!$model->hasField($deleteCol)) {
+			return false;
+		}
 
-    $model->set($deleteCol, null);
-    return $model->saveField($deleteCol, null);
-  }
+		$model->set($deleteCol, null);
+		return $model->saveField($deleteCol, null);
+	}
 }
--- Test/Case/Model/Behavior/DeletedAtBehaviorTest.php
+++ Test/Case/Model/Behavior/DeletedAtBehaviorTest.php
@@ -9,90 +9,90 @@ class DeletedUser extends CakeTestModel {
  *
  * @var string
  */
-  public $name = 'DeletedUser';
+	public $name = 'DeletedUser';
 
 /**
  * useDbConfig property
  *
  * @var array
  */
-  public $useDbConfig = 'test';
+	public $useDbConfig = 'test';
 
 /**
  * validate property
  *
  * @var array
  */
-  public $validate = array('user' => 'notEmpty', 'password' => 'notEmpty');
+	public $validate = array('user' => 'notEmpty', 'password' => 'notEmpty');
 
 }
 
 class DeletedAtBehaviorTest extends CakeTestCase {
 
-  public $fixtures = array(
-    'plugin.deleted_at.deleted_user', 'core.user'
-  );
+	public $fixtures = array(
+		'plugin.deleted_at.deleted_user', 'core.user'
+	);
 
-  public function setUp() {
-    parent::setUp();
-    $this->DeletedUser = ClassRegistry::init('DeletedUser');
-    $this->DeletedUser->useTable = 'deleted_users';
-    $this->DeletedUser->Behaviors->load('DeletedAt.DeletedAt');
-  }
+	public function setUp() {
+		parent::setUp();
+		$this->DeletedUser = ClassRegistry::init('DeletedUser');
+		$this->DeletedUser->useTable = 'deleted_users';
+		$this->DeletedUser->Behaviors->load('DeletedAt.DeletedAt');
+	}
 
-  public function tearDown() {
-    unset($this->DeletedUser);
-    parent::tearDown();
-  }
+	public function tearDown() {
+		unset($this->DeletedUser);
+		parent::tearDown();
+	}
 
-  public function testFindDeleted() {
-    $records = $this->DeletedUser->find('deleted');
-    $this->assertEqual(1, count($records));
-  }
+	public function testFindDeleted() {
+		$records = $this->DeletedUser->find('deleted');
+		$this->assertEqual(1, count($records));
+	}
 
-  public function testFindNonDeleted() {
-    $records = $this->DeletedUser->find('non_deleted');
-    $this->assertEqual(2, count($records));
-  }
+	public function testFindNonDeleted() {
+		$records = $this->DeletedUser->find('non_deleted');
+		$this->assertEqual(2, count($records));
+	}
 
-  public function testSoftdelete() {
-    $this->DeletedUser->softdelete(1);
-    $deleted = $this->DeletedUser->find('deleted');
-    $nonDeleted = $this->DeletedUser->find('non_deleted');
-    $this->assertEqual(1, count($deleted));
-    $this->assertEqual(2, count($nonDeleted));
+	public function testSoftdelete() {
+		$this->DeletedUser->softdelete(1);
+		$deleted = $this->DeletedUser->find('deleted');
+		$nonDeleted = $this->DeletedUser->find('non_deleted');
+		$this->assertEqual(1, count($deleted));
+		$this->assertEqual(2, count($nonDeleted));
 
-    $this->DeletedUser->softdelete(2);
-    $deleted = $this->DeletedUser->find('deleted');
-    $nonDeleted = $this->DeletedUser->find('non_deleted');
-    $this->assertEqual(2, count($deleted));
-    $this->assertEqual(1, count($nonDeleted));
+		$this->DeletedUser->softdelete(2);
+		$deleted = $this->DeletedUser->find('deleted');
+		$nonDeleted = $this->DeletedUser->find('non_deleted');
+		$this->assertEqual(2, count($deleted));
+		$this->assertEqual(1, count($nonDeleted));
 
-    $this->DeletedUser->softdelete(3);
-    $deleted = $this->DeletedUser->find('deleted');
-    $nonDeleted = $this->DeletedUser->find('non_deleted');
-    $this->assertEqual(3, count($deleted));
-    $this->assertEqual(0, count($nonDeleted));
-  }
+		$this->DeletedUser->softdelete(3);
+		$deleted = $this->DeletedUser->find('deleted');
+		$nonDeleted = $this->DeletedUser->find('non_deleted');
+		$this->assertEqual(3, count($deleted));
+		$this->assertEqual(0, count($nonDeleted));
+	}
 
-  public function testUnDelete() {
-    $this->DeletedUser->undelete(3);
-    $deleted = $this->DeletedUser->find('deleted');
-    $nonDeleted = $this->DeletedUser->find('non_deleted');
-    $this->assertEqual(1, count($deleted));
-    $this->assertEqual(2, count($nonDeleted));
+	public function testUnDelete() {
+		$this->DeletedUser->undelete(3);
+		$deleted = $this->DeletedUser->find('deleted');
+		$nonDeleted = $this->DeletedUser->find('non_deleted');
+		$this->assertEqual(1, count($deleted));
+		$this->assertEqual(2, count($nonDeleted));
 
-    $this->DeletedUser->undelete(2);
-    $deleted = $this->DeletedUser->find('deleted');
-    $nonDeleted = $this->DeletedUser->find('non_deleted');
-    $this->assertEqual(1, count($deleted));
-    $this->assertEqual(2, count($nonDeleted));
+		$this->DeletedUser->undelete(2);
+		$deleted = $this->DeletedUser->find('deleted');
+		$nonDeleted = $this->DeletedUser->find('non_deleted');
+		$this->assertEqual(1, count($deleted));
+		$this->assertEqual(2, count($nonDeleted));
 
-    $this->DeletedUser->undelete(1);
-    $deleted = $this->DeletedUser->find('deleted');
-    $nonDeleted = $this->DeletedUser->find('non_deleted');
-    $this->assertEqual(0, count($deleted));
-    $this->assertEqual(3, count($nonDeleted));
-  }
+		$this->DeletedUser->undelete(1);
+		$deleted = $this->DeletedUser->find('deleted');
+		$nonDeleted = $this->DeletedUser->find('non_deleted');
+		$this->assertEqual(0, count($deleted));
+		$this->assertEqual(3, count($nonDeleted));
+	}
 
 }
