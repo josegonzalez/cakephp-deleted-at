93e685d257a29f5498370b75ecdc5dca7e623182
Jose Diaz-Gonzalez <myaccounts@savant.be>
2013-12-21 17:09:22 -0500

    Add initial behavior scaffolding

---
+++ Model/Behavior/DeletedAtBehavior.php
@@ -0,0 +1,5 @@
+<?php
+App::uses('ModelBehavior', 'Model');
+class DeletedAtBehavior extends ModelBehavior {
+}
+?>
---
+++ Test/Case/Model/Behavior/DeletedAtBehaviorTest.php
@@ -0,0 +1,7 @@
+<?php
+App::uses('Model', 'Model');
+App::uses('AppModel', 'Model');
+require_once CAKE . 'Test' . DS . 'CASE' . DS . 'Model' . DS . 'models.php';
+class DeletedAtBehaviorTest extends CakeTestCase {
+
+}
