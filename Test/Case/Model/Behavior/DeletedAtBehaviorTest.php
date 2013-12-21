94241b298e3ba100718cb00f26528bde8ffbc359
Jose Diaz-Gonzalez <myaccounts@savant.be>
2013-12-21 17:10:35 -0500

    Add initial code for setup/teardown of test cases

--- Test/Case/Model/Behavior/DeletedAtBehaviorTest.php
+++ Test/Case/Model/Behavior/DeletedAtBehaviorTest.php
@@ -4,4 +4,20 @@ App::uses('AppModel', 'Model');
 require_once CAKE . 'Test' . DS . 'CASE' . DS . 'Model' . DS . 'models.php';
 class DeletedAtBehaviorTest extends CakeTestCase {
 
+  public $fixtures = array(
+    'plugin.deleted_at.deleted_user', 'core.user'
+  );
+
+  public function setUp() {
+    parent::setUp();
+    $this->DeletedUser = ClassRegistry::init('User');
+    $this->DeletedUser->useTable = 'deleted_users';
+    $this->DeletedUser->Behaviors->load('DeletedAt.DeletedAt');
+  }
+
+  public function tearDown() {
+    unset($this->DeletedUser);
+    parent::tearDown();
+  }
+
 }
